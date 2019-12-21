61d5956a0ba40d7cf63a0e2024943805f2b8ea0e
Cory Gilliam <imaaxa@gmail.com>
2019-12-21 17:58:33 -0500

    Assignment 1.5

---
+++ Week-1/gilliam-hello-modules/src/app/shipping/shipping.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-shipping',
+  template: `<p>Welcome from the ShippingComponent.</p>`,
+  styles: []
+})
+export class ShippingComponent implements OnInit {
+  constructor() { }
+  ngOnInit() {}
+}

7d1adb82988a518c7470b54aef937cc18aae4d4c
Cory Gilliam <imaaxa@gmail.com>
2020-01-31 09:33:53 -0500

    Form validation working.

--- Week-5/gilliam-md-in-action/src/app/app.module.ts
+++ Week-5/gilliam-md-in-action/src/app/app.module.ts
@@ -1,19 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
-import {
-  MatButtonModule,
-  MatCardModule,
-  MatFormFieldModule,
-  MatIcon,
-  MatIconModule,
-  MatInputModule,
-  MatMenuModule,
-  MatToolbarModule
-} from "@angular/material";
-import { FlexLayoutModule } from "@angular/flex-layout";
-import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
+// Form Inports
+import { MatButtonModule } from "@angular/material/button";
+import { MatCardModule } from "@angular/material/card";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatIcon } from "@angular/material/icon";
+import { MatIconModule } from "@angular/material/icon";
+import { MatInputModule } from "@angular/material/input";
+import { MatMenuModule } from "@angular/material/menu";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { ReactiveFormsModule } from "@angular/forms";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+
+// Layout
+import { FlexLayoutModule } from "@angular/flex-layout";
+
+// Angular Component Imports
+import { AppComponent } from './app.component';
+import { NgModule } from '@angular/core';
 import { MenuComponent } from './menu/menu.component';
 import { LoginComponent } from './login/login.component';
 
@@ -33,7 +37,8 @@ import { LoginComponent } from './login/login.component';
     MatInputModule,
     MatMenuModule,
     MatToolbarModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Week-5/gilliam-md-in-action/src/app/login/login.component.ts
+++ Week-5/gilliam-md-in-action/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  // FormGroup object
+  myForm: FormGroup;
+
+  // Add the FormGroup object
+  constructor( private fb: FormBuilder ) { }
 
   ngOnInit() {
+    this.myForm = this.fb.group({
+      // Defalut field values and validators
+      userName: ['', [
+        Validators.required
+      ]],
+      passWord: ['', [
+        Validators.required,
+        Validators.pattern('^(?=.*[0-9])(?=.*[A-Z])(?=.*[a-z])(?=.*[!?@#$%^&+=])([a-zA-Z0-9!?@#$%^&+=]+){8,}$')
+      ]]
+    });
+
+    // Subscribe to form changes.
+    this.myForm.valueChanges.subscribe(console.log);
   }
 
+  // Formfield getters
+  get userName() {
+    return this.myForm.get('userName');
+  }
+
+  get passWord() {
+    return this.myForm.get('passWord');
+  }
 }

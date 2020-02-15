ef25e3554ae4392b2a015e43f24b943c7836a6e1
Cory Gilliam <imaaxa@gmail.com>
2020-02-15 14:20:57 -0500

    Exercise 7.2

--- Week-7/gilliam-template-forms/src/app/app.component.ts
+++ Week-7/gilliam-template-forms/src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Gilliam Template Forms';
+
+  onSubmit(formData) {
+    console.log(formData);
+  }
 }
--- Week-7/gilliam-template-forms/src/app/app.module.ts
+++ Week-7/gilliam-template-forms/src/app/app.module.ts
@@ -1,33 +1,31 @@
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { NgModule } from '@angular/core';
 
-// Component
-import { AppComponent } from './app.component';
-
-// Forms
-import { FormsModule } from "@angular/forms";
-import { MatButtonModule } from '@angular/material/button'
-import { MatInputModule } from '@angular/material/input'
-import { MatFormFieldModule } from '@angular/material/form-field'
-import { MatCardModule } from '@angular/material/card'
-
-// Layout
 import { FlexLayoutModule } from "@angular/flex-layout";
+
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
     BrowserAnimationsModule,
+    BrowserModule,
     FlexLayoutModule,
     FormsModule,
     MatButtonModule,
-    MatInputModule,
+    MatCardModule,
     MatFormFieldModule,
-    MatCardModule
+    MatInputModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
